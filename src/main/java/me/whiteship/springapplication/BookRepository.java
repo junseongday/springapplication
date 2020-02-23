d36bd43a5785be030a090e2ece0f9b37dde76d00
junseongday <junseongday@gmail.com>
2020-02-23 12:46:33 +0900

    스프링2.5부터 가능한 componentScan

--- src/main/java/me/whiteship/springapplication/BookRepository.java
+++ src/main/java/me/whiteship/springapplication/BookRepository.java
@@ -1,4 +1,7 @@
 package me.whiteship.springapplication;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class BookRepository {
 }
--- src/main/java/me/whiteship/springapplication/BookService.java
+++ src/main/java/me/whiteship/springapplication/BookService.java
@@ -1,6 +1,12 @@
 package me.whiteship.springapplication;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService  {
+    @Autowired
     BookRepository bookRepository;
 
     public void setBookRepository(BookRepository bookRepository) {
